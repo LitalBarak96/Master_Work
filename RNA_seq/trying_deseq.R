22988933923f71c1ea1dd3eed1c031a653badb42
Lital Barak <lital.barak@live.biu.ac.il>
2021-11-09 16:34:39 +0200

    started the generalization of creat exp

---
+++ RNA_seq/trying_deseq.R
@@ -0,0 +1,41 @@
+cts <- as.matrix(read.csv("D:/RNA_seq/20210608GalitOphir-270838574/SALMON_1.5.2/summery/DEseq/Salmon_TPM.csv",sep=",",row.names="gene_id"))
+coldata <- read.csv("D:/RNA_seq/20210608GalitOphir-270838574/SALMON_1.5.2/summery/DEseq/meta_test.csv", row.names=1)
+coldata$condition <- factor(coldata$condition)
+rownames(coldata) <- sub("fb", "", rownames(coldata))
+all(rownames(coldata) %in% colnames(cts))
+all(rownames(coldata) == colnames(cts))
+cts <- cts[, rownames(coldata)]
+all(rownames(coldata) == colnames(cts))
+
+
+library("DESeq2")
+dds <- DESeqDataSetFromMatrix(countData = round(cts),
+                              colData = coldata,
+                              design = ~ condition)
+dds
+
+featureData <- data.frame(gene=rownames(cts))
+mcols(dds) <- DataFrame(mcols(dds), featureData)
+mcols(dds)
+
+
+
+#removing low count genes
+#keep <- rowSums(counts(dds)) >= 2
+#dds <- dds[keep,]
+
+#check this is put the condition right
+#dds$condition <- factor(dds$condition, levels = c("males","females"))
+
+dds <- DESeq(dds)
+resultsNames(dds)
+res <- results(dds)
+
+keep <- rowSums(counts(dds)) > 0
+dds <- dds[keep,]
+
+vsdata <-vst(dds,blind=FALSE)
+library(autoplotly)
+library(ggplot2)
+autoplotly::autoplotly((plotPCA(vsdata, intgroup="condition")+geom_text(aes(label=name),vjust=2)+labs(title = "PCA: male vs. female"))
+                       , data = vsdata, colour = 'Species', frame = TRUE)
--- test_for_chosing_colors_and_passing.m
+++ test_for_chosing_colors_and_passing.m
@@ -1,14 +1,32 @@
-command = '"C:\Program Files\R\R-4.0.4\bin\x64\Rscript.exe" createExperimentNetworks3pop.R';
+command = '"C:\Program Files\R\R-4.1.2\bin\x64\Rscript.exe" test_pass.R ';
+color ="";
+num_of_pop =2;
+group_name =[]
+color_value=[];
 
-for i =1:3
+for i =1:num_of_pop
 s1='Select a color for group number ';
 s2 = int2str(i);
+group_name= [group_name;s2];
 s = append(s1,s2);
 c = uisetcolor([1 1 0],s)
 color_in_char =[];
-B=[];
 color_in_char= sprintf(' %f', c)
-B = color_in_char
-command= append(command,B)
+color_value = [color_value;c];
+%color = append(color,B)
 end
+tables=table(group_name,color_value);
+OriginFolder = pwd;
+dname = uigetdir();
+folder = dname;
+if ~exist(folder, 'dir')
+    mkdir(folder);
+end
+cd(dname)
+baseFileName = 'color.xlsx';
+path_of_xlsx = fullfile(folder, baseFileName);
+writetable(tables,baseFileName)
+
+cd(OriginFolder)
+command = append(command,path_of_xlsx) 
 [status,cmdout]=system(command,'-echo');
