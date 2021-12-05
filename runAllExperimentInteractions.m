20a3ab817d7f5aad9b0435ca45f915a21b15e3ee
Lital Barak <lital.barak@live.biu.ac.il>
2021-12-05 14:56:22 +0200

    checked and network working

--- runAllExperimentInteractions.m
+++ runAllExperimentInteractions.m
@@ -11,11 +11,54 @@ param.directed = false;
 param.jaabaFileName = 'registered_trx.mat';
 maxExperimentInteractions = -1;
 
-
+command = '"C:\Program Files\R\R-4.1.2\bin\x64\Rscript.exe" creatExpNet.R ';
 expGroups = uipickfiles('Prompt', 'Select experiment groups folders');
 [suggestedPath, ~, ~] = fileparts(expGroups{1});
 savePath = uigetdir(suggestedPath, 'Select folder to save csv file');
 numOfGroups = length(expGroups);
+
+prompt = {'insert height window size','insert width window size','insert font size','insert asterisk size'};
+dlgtitle = 'parameters';
+dims = [1 35];
+definput = {'12','12','5','3'};
+opts.Resize = 'on';
+answer_size = inputdlg(prompt,dlgtitle,dims,definput,opts);
+height= str2num(answer_size{1});
+width =str2num(answer_size{2});
+font =str2num(answer_size{3});
+asterisk =str2num(answer_size{4});
+
+answer_change = questdlg('Would you like to run from the beginning or only change visual?', ...
+	'run or vizual', ...
+	'change vizual','run from the beginning','run from the beginning');
+% Handle response
+switch answer_change
+    case 'change vizual'
+        warndlg(' you choose change vizual  ','this is not and error')
+        disp([answer_change ' start running vizual soon '])
+        change = 2;
+    case 'run from the beginning'
+        warndlg(' calculating from beginning ','this is not error')
+        disp([answer_change ' lets start from the beginning '])
+        change = 1;
+end
+
+answer_delete = questdlg('wish to delete paramters files at the end?', ...
+	'delete or keep', ...
+	'delete','keep','keep');
+% Handle response
+switch answer_delete
+    case 'delete'
+        warndlg(' will be deleted ','this is not error')
+        disp([answer_delete ' will be deleted '])
+        deleted = 1;
+    case 'keep'
+        warndlg(' we wont delete ','this is not error')
+        disp([answer_delete ' wont be deleted '])
+        deleted = 0;
+end
+
+if (change == 1)
 xlsxFileName = fullfile(savePath, ['expData_', num2str(param.startFrame), '_to_', num2str(param.endFrame), '.xlsx']);
 groupNumber = [{'Number of groups'; numOfGroups}; cell(numOfGroups - 1, 1)];
 groupNames = cell(numOfGroups + 1, 1);
@@ -49,55 +92,33 @@ maxInter = cell(size(data, 1), 1);
 maxInter{1} = 'Max number of interaction';
 maxInter{2} = maxExperimentInteractions;
 data = [data, maxInter];
-
 xlswrite(xlsxFileName, data);
-[num,txt,raw] = xlsread(xlsxFileName);
-command = '"C:\Program Files\R\R-4.1.2\bin\x64\Rscript.exe" creatExpNet.R ';
-prompt = {'how many population:'};
-dlgtitle = 'number of population';
-dims = [1 35];
-definput = {'2'};
-answer = inputdlg(prompt,dlgtitle,dims,definput)
-num_of_pop=str2double(char(answer))
-
-prompt = {'insert height window size','insert width window size','insert font size','insert asterisk size','wish to delete the paramters file?(1-yes,0-no if no please remmber delete manually)'};
-dlgtitle = 'Input';
-dims = [1 35];
-definput = {'12','12','5','3','0'};
-answer_size = inputdlg(prompt,dlgtitle,dims,definput)
-
-
-height= str2num(answer_size{1});
-width =str2num(answer_size{2});
-font =str2num(answer_size{3});
-asterisk =str2num(answer_size{3});
-delete =str2num(answer_size{5});
+end
 
 color ="";
-group_name =[]
-color_value=[];
+groupNameDir =[];
+colorValue=[];
 
+groupNameDir=expGroups';
 
-for i =1:num_of_pop
-s1='Select a color current group ';
-s2 = uigetdir('C:\','choose the directory where the files of the experiment located');
-group_name=strvcat(group_name,s2)
-c = uisetcolor([1 1 0],s1)
+for i =1:numOfGroups
+s1='Select a color for ';
+[~,currentGroupName,~]=fileparts(groupNameDir(i));
+displayOrder =char(strcat(s1,{' '},currentGroupName));
+c = uisetcolor([1 1 0],displayOrder);
 color_in_char =[];
 color_in_char= sprintf(' %f', c)
-
-color_value = [color_value;c];
+colorValue = [colorValue;c];
 end
-tables=table(group_name,color_value);
-tables_params=table(height,width,font,asterisk,delete);
+tables=table(groupNameDir,colorValue);
+tables_params=table(height,width,font,asterisk,change,deleted);
 
 OriginFolder = pwd;
-dname = uigetdir('C:\','please choose temporary directory to save paramter files');
-folder = dname;
+folder=savePath;
 if ~exist(folder, 'dir')
     mkdir(folder);
 end
-cd(dname)
+cd(folder)
 baseFileName = 'color.xlsx';
 path_of_xlsx = fullfile(folder, baseFileName);
 writetable(tables,baseFileName)
