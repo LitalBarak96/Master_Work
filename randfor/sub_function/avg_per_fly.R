a110e1b85dd602e8e1280def8b11ed0748403470
Lital Barak <lital.barak@live.biu.ac.il>
2022-03-21 13:45:24 +0200

    fixed per fly

--- randfor/sub_function/avg_per_fly.R
+++ randfor/sub_function/avg_per_fly.R
@@ -1,5 +1,6 @@
 avg_per_fly<-function(dir,path_to_scripts){
-  current_dir =getwd()
+  
+  current_dir =dir
   setwd(path_to_scripts)
   files.sources = list.files()
   sapply(files.sources, source)
@@ -22,6 +23,7 @@ avg_per_fly<-function(dir,path_to_scripts){
     index<-0
     for(j in 1:length(files)){ 
       if (files[j] %in% included){
+        row_ave.df<-data.frame()
         index<-index+1
         df <- readMat(paste0(curr.dir, '/', files[j])) # read each MAT file
         #print(j)
@@ -34,21 +36,16 @@ avg_per_fly<-function(dir,path_to_scripts){
         tmp_movie_ave.df<-data.frame(dir=dir[k], file=files[j], value=mean(row_ave.df$value))
         
         if (index==1){
-          movie_ave.df<-data.frame(dir=dir[k], file=files[j], value=mean(row_ave.df$value)) #make average per movie
           col.df<- row_ave.df
         }else{
-          col.df<- cbind(col.df, row_ave.df)
-          movie_ave.df<-cbind(movie_ave.df, tmp_movie_ave.df) #combine averages per movie
+          col.df<- bind_cols(col.df, row_ave.df)
         }
-        row_ave.df<- NULL
       }
     }
     if (k==1){
-      total_movie_ave.df<- movie_ave.df
       all_col.df<-col.df
     }else{
-      total_movie_ave.df<-rbind(total_movie_ave.df, movie_ave.df)
-      all_col.df<-cbind(all_col.df, col.df)
+      all_col.df<-bind_rows(all_col.df, col.df)
     }
     
     #ordered_ave<- col.df[order(col.df$file),]
