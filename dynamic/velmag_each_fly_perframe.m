e83f4e9109031efe6399c82013701a30989020f5
Lital Dvora Buchris Barak <barakli8@biu.ac.il>
2022-05-31 14:08:42 +0300

    created matlab script that calculte all per frame feature avarge per frame

--- dynamic/velmag_each_fly_perframe.m
+++ dynamic/velmag_each_fly_perframe.m
@@ -1,5 +1,25 @@
 %getting the first value in each cell
-path="D:\allGroups\Females_Grouped\Assa_Females_Grouped__Unknown_RigA_20220206T090743";
+
+%PATH TO THE PER FRAME 
+path="D:\allGroups\Females_Mated\Assa_Female_Mated_Unknown_RigA_20211025T120646\perframe";
+
+%subfile to get the names
+perframedir=dir(fullfile(path));
+%changge to struct to get accses
+cell_dir=struct2cell(perframedir);
+all=[];
+
+%the 1 and 2 are the current dir 
+for j =3:81
+name=cell_dir{1,j}
+load(fullfile(path,cell_dir{1,j}))
+
+
+%name_of_ferframe=zeros(1,81)
+
+%for i=1:81
+ %   name_of_ferframe(i)=cell_dir{1,i}
+%end
 
 num_of_frames=27001;
 perframe_avarge_allflies=zeros(1,num_of_frames);
@@ -8,7 +28,15 @@ for i=1:num_of_frames
 perframe_avarge_allflies(i)=sum_per_frame;
 end
 horizen_perframe_avarge_allflies=perframe_avarge_allflies'
-name_of_file="velmag_perframe_sum_allflies.csv"
 
-full_path_tocsv=fullfile(path,name_of_file)
-csvwrite(full_path_tocsv,horizen_perframe_avarge_allflies)
+table_of_current_perframe = array2table(horizen_perframe_avarge_allflies, 'VariableNames',{name});
+
+all=horzcat(all,table_of_current_perframe);
+
+end
+
+
+filename="perframe_sum_allflies.csv";
+
+full_path_tocsv=fullfile(path,filename)
+writetable(all,full_path_tocsv)
