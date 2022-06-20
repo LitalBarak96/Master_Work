2d23709913b9624c9e80d09d7f6142af10de53c2
Lital Dvora Buchris Barak <barakli8@biu.ac.il>
2022-06-20 12:32:28 +0300

    added pval to the pipe,saving it as needed

--- Hcluster/litalHcluster/run_and_Creat_hclsuter.R
+++ Hcluster/litalHcluster/run_and_Creat_hclsuter.R
@@ -26,16 +26,18 @@ run_and_Creat_hclsuter<-function(full_path_to_dirs,path_to_scripts){
   sapply(files.sources, source)
   setwd(current_dir)
 
-
+#the color and the name of the groups(even if we don't use the color)
 allDirsData <- read.xlsx(full_path_to_dirs)
+#to get the number of population in the comperence
 num_of_pop<<-nrow(allDirsData)
+#the parent dir
 param_dir = tools::file_path_sans_ext(dirname((full_path_to_dirs)))
 
 
 
 
 
-
+#get data frame that conatin dynamicly the path to all the sub population
 
 dir=as.data.frame(lapply(structure(.Data=1:1,.Names=1:1),function(x) numeric(num_of_pop)))
 for (i in 1:num_of_pop){
@@ -43,6 +45,8 @@ for (i in 1:num_of_pop){
   dir[i,1]<-str_trim(dir[i,1], side = c("right"))
 }
 
+
+#find if the dirs exist "sinaty check"
 dir$X1<-gsub("\\\\", "/", dir$X1)
 for(i in 1:num_of_pop){
   if(dir.exists(dir[i,1])){
@@ -50,18 +54,21 @@ for(i in 1:num_of_pop){
   }
 }
 
+#get the subgroup names
 groupsNames <- as.character(basename(allDirsData$groupNameDir))
 
 
 setwd(param_dir)
 ## need to save the file in the perent folder
+#sometimes there is dup of features with ~,this remove it
 file_check_not_doubled<-as.data.frame(read.xlsx("files_for_dup.xlsx"))
-#anothertest<-as.data.frame(read.csv("F:/allGroups/Females_Grouped/averages per condition.csv"))
 all_together<-data.frame()
 num_of_removed_valus<-0
 for(i in 1:num_of_pop){
   full_path_avg_per_con<-paste0(dir[i,1],"\\","averages per condition.csv")
+  #read the csv
   df<-as.data.frame(read.csv(full_path_avg_per_con))
+  #remove the dubled ,there is ~ somehow in there
   show_remove<-anti_join(file_check_not_doubled,df , by = "file")
   if(i!=1 & num_of_removed_valus!=nrow(show_remove)){
     
@@ -74,7 +81,7 @@ for(i in 1:num_of_pop){
   }
 
   
-
+  #styling the names 
   df$file<- str_replace(df$file, "scores_", "")
   df$file<-gsub("Aggregation", "Social Clustering", df$file)
   df$file<-gsub("Interaction_Assa", "Approach", df$file)
@@ -90,19 +97,27 @@ for(i in 1:num_of_pop){
 
 
 
+##all together is ready and now we creating the heatmap itself
 
-#numric_together<-as.matrix(all_together)
-#heatmap(numric_together, scale = "none")
+#install.packages("pvclust")
 
-
+library(pvclust)
 
 library("pheatmap")
+
 library(seriation)
 library(dendextend)
 library(svDialogs)
 phtmap <- pheatmap(all_together)
 row_dend <- phtmap[[1]]
 #sorting by rows and alphbeticly
+
+
+
+
+result <- pvclust(all_together, method.dist="euclidian", method.hclust="average", nboot=1000, parallel=TRUE)
+plot(result)
+pvrect(result, alpha=0.95)
 
 change <- dlg_list(c("yes","no"), multiple = TRUE,title="change heatmap name order?",Sys.info()
 )$res
@@ -116,38 +131,27 @@ if(change == "yes"){
   t<-pheatmap(all_together, cluster_rows =as.hclust(row_dend))
   setwd((choose.dir(caption = "Select folder for saving the heatmap")))
   ggsave(plot = t, filename = "heatmap.jpeg", units = "cm")
+  jpeg(filename = "dendrogram_p_val.jpeg", width = 1200, height = 800)
+  plot(result)
+  pvrect(result, alpha=0.95)
+  dev.off()
+  
 }else{
   t <- pheatmap(all_together)
   setwd((choose.dir(caption = "Select folder for saving the heatmap")))
   ggsave(plot = t, filename = "heatmap.jpeg",units = "cm")
-  
+  jpeg(filename = "dendrogram_p_val.jpeg", width = 1200, height = 800)
+  plot(result)
+  pvrect(result, alpha=0.95)
+  dev.off()
+
 }
-#list_of_names<-c("Males_Grouped","Males_Mated","Males_Singels","Females_Mated","Females_Grouped","Females_Singles")
-
-
-#row_dend <- rotate(row_dend, order = sort(rownames(all_together)[get_order(row_dend)]))
-#row_dend<-rotate(row_dend,res)
-#pheatmap(all_together, cluster_rows =as.hclust(row_dend))
-
-#pheatmap(all_together,cluster_rows = FALSE,show_colnames  = T)
-
 
 # Finding distance matrix
 distance_mat <- dist(all_together, method = 'euclidean')
 distance_mat
 
-# Fitting Hierarchical clustering Model
-# to training dataset
-set.seed(240) # Setting seed
-Hierar_cl <- hclust(distance_mat, method = "average")
-Hierar_cl
-plot(Hierar_cl)
 
-# Plotting dendrogram
-
-# Choosing no. of clusters
-# Cutting tree by height
-abline(h = 110, col = "green")
 
 
 }
