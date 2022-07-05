25e6f0afa6ff2c055b23ae17c68a246fef95d1d3
Lital Dvora Buchris Barak <barakli8@biu.ac.il>
2022-07-05 15:50:28 +0300

    added comments on shir code for interaction matrix and also created histogram for intearction matrix in r

--- dynamic/interactionFrameMatrix_main_without_angelsub.m
+++ dynamic/interactionFrameMatrix_main_without_angelsub.m
@@ -38,3 +38,35 @@ for i = 1:numOfGroups
     runOneGroupInteractions_new(param, allFolders, groupName);
 
 end
+
+
+%% 
+
+jaabaFileName = 'registered_trx.mat';
+%jaabaFileName = 'ctrax_results - Copy.mat';
+
+
+param = struct();
+param.interactionsNumberOfFrames = 60;
+param.interactionsDistance = 8;
+param.interactionsAnglesub = 0;
+param.oneInteractionThreshold = 120;
+param.startFrame = 0;
+param.endFrame = 27000;
+%i can change here to false and get undirected network parametrs
+param.directed = false;
+%param.interactionLength = true;
+
+interactions = [];
+noInteractions = [];
+
+handles.allFolders = uipickfiles('Prompt', 'Select movies to run inteactions');
+for i = 1:length(handles.allFolders)
+    folderPath = handles.allFolders{i};
+    fileName = fullfile(folderPath, jaabaFileName);
+    [COMPUTERPERFRAMESTATSSOCIAL_SUCCEEDED,savenames] = compute_perframe_stats_social_f('matname', fileName);
+    [newInteractions, newNoInteractions, txtFileName] = computeAllMovieInteractions_new(savenames, param);
+    interactions = [interactions, newInteractions];
+    noInteractions = [noInteractions, newNoInteractions];
+end
+
