0c57af64f24927c05e896fe73eeb40df4f4cc13f
Lital Dvora Buchris Barak <barakli8@biu.ac.il>
2023-03-02 11:33:39 +0200

    aaded psate inseated change dir

--- ScatterPlot_final/scatter_source/scaleing.R
+++ ScatterPlot_final/scatter_source/scaleing.R
@@ -8,15 +8,15 @@ scaleing<-function(csv_file_name,dir,path_to_scripts){
   
 
   temp<-data.frame()
-  setwd(dir[1,1])
-  comb<-as.data.frame(read.csv(csv_file_name))
+  #setwd(dir[1,1])
+  comb<-as.data.frame(read.csv(paste(dir[1,1],"/",csv_file_name,sep = "")))
   num_of_movies<-length(list.dirs(path=dir[1,1], recursive=F ))
   v <- rep(tools::file_path_sans_ext(basename(((dir[1,1])))), num_of_movies)
   library("dplyr") # or library("tidyverse")
   comb <- cbind(id = v,comb)
   for(i in 2:num_of_pop){
-    setwd(dir[i,1])
-    temp<-as.data.frame(read.csv(csv_file_name))
+    #setwd(dir[i,1])
+    temp<-as.data.frame(read.csv(paste(dir[i,1],"/",csv_file_name,sep = "")))
     num_of_movies<-length(list.dirs(path=dir[i,1], recursive=F ))
     v <- rep(tools::file_path_sans_ext(basename(((dir[i,1])))), num_of_movies)
     temp <- cbind(id = v,temp)
